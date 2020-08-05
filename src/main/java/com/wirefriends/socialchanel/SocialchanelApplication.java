0cf879bfa1420a4986797c658e0410460ca7797a
John Bonilla <jsbonilla2600@gmail.com>
2020-08-04 23:41:12 -0500

    first commit

---
+++ src/main/java/com/wirefriends/socialchanel/SocialchanelApplication.java
@@ -0,0 +1,13 @@
+package com.wirefriends.socialchanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialchanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialchanelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wirefriends/socialchanel/SocialchanelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wirefriends.socialchanel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialchanelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
